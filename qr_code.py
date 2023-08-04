a5e33c9384c4cceb38968d6efe225dde37594c09
Akash MAhapatra <84181582+akxsh-dev@users.noreply.github.com>
2023-08-04 21:21:36 +0530

    Add files via upload

---
+++ qr_code.py
@@ -0,0 +1,3 @@
+import qrcode as qr
+img= qr.make("https://www.linkedin.com/in/thisisakash/")
+img.save("akash_linkedin.png")
---
+++ qr_code_2.py
@@ -0,0 +1,13 @@
+import qrcode
+from PIL import Image
+
+qr=qrcode.QRCode(version=1,
+                 error_correction=qrcode.constants.ERROR_CORRECT_H,
+                 box_size=10,
+                 border=4,
+                 )
+
+qr.add_data("https://www.linkedin.com/in/thisisakash/")
+qr.make(fit=True)
+img=qr.make_image(fill_color="red",back_color="black")
+img.save("MylinkedInProfile.png")
